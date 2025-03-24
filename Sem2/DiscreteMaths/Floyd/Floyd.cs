cbb88e8cf7fa44d8c2a99507f6cd5d19148323e9
ShatilovIvan <ivashka1423@mail.ru>
2025-03-25 00:15:07 +0600

    feat: Floyd algorithm

---
+++ Sem2/DiscreteMaths/Floyd/Floyd.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+class Program
+{
+    static int[,] Floyd(int[,] graph)
+    {
+        int verticesCount = graph.GetLength(0);
+
+        for (int k = 0; k < verticesCount; k++)
+        {
+            for (int i = 0; i < verticesCount; i++)
+            {
+                for (int j = 0; j < verticesCount; j++)
+                {
+                    if (graph[i, k] != int.MaxValue && graph[k, j] != int.MaxValue)
+                    {
+                        graph[i, j] = Math.Min(graph[i, k] + graph[k, j], graph[i, j]);
+                    }
+                }
+            }
+        }
+
+        return graph;
+    }
+
+    static void Print(int[,] graph)
+    {
+        int verticesCount = graph.GetLength(0);
+
+        for (int i = 0; i < verticesCount; i++)
+        {
+            for (int j = 0; j < verticesCount; j++)
+            {
+                Console.Write($"{graph[i,j]}\t");
+            }
+
+            Console.WriteLine();
+        }   
+    }
+
+    static void Main()
+    {
+        int[,] graph = 
+        {
+            {0, 5, 10, int.MaxValue},
+            {int.MaxValue, 0, 3, 13},
+            {int.MaxValue, int.MaxValue, 0, 8},
+            {7, 15, 20, 0}
+        };
+
+        Print(Floyd(graph));
+    }
+}
