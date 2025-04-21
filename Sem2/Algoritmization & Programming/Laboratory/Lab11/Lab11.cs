11b1230d16e09d7bd7940dd2bd7f37603c50c27b
Shatilov Ivan <ivashka1423@mail.ru>
2025-04-21 14:43:40 +0300

    feat: laboratory 21.04

---
+++ Sem2/Algoritmization & Programming/Laboratory/Lab11/Lab11.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class Program
+{
+	static void Main()
+	{
+		string input = "input.txt";
+		string output = "output.txt";
+
+		var lines = File.ReadAllLines(input);
+		var newLines = new List<string>();
+
+		foreach (var line in lines)
+		{
+			string number = "";
+
+			foreach (char c in line)
+			{
+				if (char.IsDigit(c))
+				{
+					number += c;
+				}
+
+				else
+				{
+					if (number.Length > 0)
+					{
+						if (int.Parse(number) % 2 == 0)
+						{
+							newLines.Add(line);
+							number = "";
+							continue;
+						}
+
+					}
+
+				}
+
+			}
+
+			if (number.Length > 0)
+			{
+				if (int.Parse(number) % 2 == 0)
+				{
+					newLines.Add(line);
+					number = "";
+				}
+
+			}
+
+		}
+
+		File.WriteAllLines(output, newLines);
+	}
+}
